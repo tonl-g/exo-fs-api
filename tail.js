b287ac55e7e19a6ea9b5f6a7fa56fb58a73fcc35
tonl-g <tlygonneville.dev@gmail.com>
2021-03-25 18:28:10 +0100

    Modif

--- tail.js
+++ tail.js
@@ -3,25 +3,3 @@ Affiche les 10 dernières lignes du fichier file.txt.
 Gestion de l'erreur si pas le bon nombre d'arguments.
 Gestion de l'erreur si file.txt n'existe pas. */
 
-const fs = require('fs') // Importe tout le module FS
-
-// Vérifier la command line
-if (process.argv.length !==3) {
-    console.log(`usage: node tail.js file.txt`)
-    process.exit(1)
-}
-
-// Check if file exists
-if (!fs.existsSync(process.argv[2])) {
-    console.log(`Error: ${process.argv[2]} does not exist`)
-    process.exit(1)
-}
-
-let array = fs.readFileSync('file.txt').toString().split(" ")
-for(i in array) { 
-    console.log(array[i])
-}
-
-// let lastContent = fs.readFileSync(process.argv[2], 'utf-8')
-
-// console.log(lastContent)
